298b260c54051c56284d8f29d8ea305d7430e21d
IBAR <brigi.istvan@gmail.com>
2026-09-14 11:47:57 +0200

    Fix SHELL array syntax in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'malomerdo-elszamolo-v1';
-const SHELL = ['./malomerdo-elszamolo.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const SHELL = ["start_url":"./index.html", './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL)));
